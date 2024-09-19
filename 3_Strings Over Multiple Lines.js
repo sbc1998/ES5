ce36263abb0ac02d9e4c106a467dd7124c4e19a2
sbc1998 <155535756+sbc1998@users.noreply.github.com>
2024-09-19 14:50:52 +0545

    Create 3_Strings Over Multiple Lines.js

---
+++ 3_Strings Over Multiple Lines.js
@@ -0,0 +1,24 @@
+console.log("Template Literals:");
+const string1 = `Avicii 
+Martin    
+Zedd`;
+console.log(string1);
+
+const string2 = `apple \
+banana \
+cherries`;
+console.log(string2);
+
+console.log("\nNewline Escaping:");
+const string3= "jeep\nmustang\nford";
+console.log(string3);
+
+console.log("\nString concatenation:");
+const string4= "Forest " + "Rebecca " + "Sarah";
+const string5= "Forest\n" + "Rebecca\n" + "Sarah";
+console.log(string4);
+console.log(string5);
+
+
+
+
