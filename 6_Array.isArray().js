49a2c2f82dbfd7674fd218a88b62aef263238ce6
Shashank B C <bcshashank140@gmail.com>
2024-09-24 02:24:17 +0545

    Create 6_Array.isArray().js
    The isArray() method checks whether the passed argument is an array or not.

---
+++ 6_Array.isArray().js
@@ -0,0 +1,9 @@
+// Array.isArray()
+
+let artist= ["Avicii", "Martin Garrix", "Zedd"];
+let result= Array.isArray(artist);
+console.log(result);
+
+let text= "python";
+let result2= Array.isArray(text);
+console.log(result2);
