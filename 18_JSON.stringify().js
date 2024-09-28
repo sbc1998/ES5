0862dd7ea3562b75188ac661aea9e8d15e85acf9
Shashank B C <bcshashank140@gmail.com>
2024-09-28 23:39:07 +0545

    Create 18_JSON.stringify().js
    A JSON is also used to send data to a web server and the data has to be a string. Therefore, JSON.stringify() is used to convert it into a string.

---
+++ 18_JSON.stringify().js
@@ -0,0 +1,10 @@
+
+const info= {
+    name: "Martin", 
+    age: 25, 
+    country: "Netherlands"
+};
+
+const string= JSON.stringify(info);// converts info ojbect into string
+console.log(string);
+
