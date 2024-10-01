73a59648c712435dee7cba33fa9f72651402638c
Shashank B C <bcshashank140@gmail.com>
2024-10-01 16:42:30 +0545

    Update 20_Property Getters.js
    getter methods are used to access the properties of an object.

--- 20_Property Getters.js
+++ 20_Property Getters.js
@@ -1,19 +1,10 @@
-const student = {
-
-    // data property
-    firstName: 'Monica',
-    
-    // accessor property(getter)
-    get getName() {
-        return this.firstName;
+const person= {
+    fname: "Tim",
+    lname: "Bergling",
+    age: 25,
+    get name() {
+        return this.fname.toUpperCase() + " " + this.lname.toUpperCase();
     }
-};
+}
 
-// accessing data property
-console.log(student.firstName); // Monica
-
-// accessing getter methods
-console.log(student.getName); // Monica
-
-// trying to access as a method
-console.log(student.getName()); // error
+console.log(person.name);
